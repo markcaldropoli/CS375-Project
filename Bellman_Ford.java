c7b98e487e01b404b18422ae2eb16325447c0d21
Marc Shelton <mshelto3@binghamton.edu>
2018-11-29 16:54:28 -0500

    Made Bellman_Ford work with Edge class

--- Bellman_Ford.java
+++ Bellman_Ford.java
@@ -1,16 +1,17 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Bellman_Ford {
 	private int V;
-	private int d[];
+	private int[] d;
 	public Bellman_Ford(int V) {
 		this.V = V;
 		d = new int[V+1];
 	}
-	public int[] BellmanFord_Matrix(int start, int matrix[][]) {
+	public int[] BellmanFord_Matrix(int start, int[][] matrix) {
 		Arrays.fill(d, Integer.MAX_VALUE);
 		d[start] = 0;
-		for(int i = 1; i < V-1; i++) {
+		for(int i = 1; i <= V-1; i++) {
 			for(int source = 1; source <= V; source++) {
 				for(int dest = 1; dest <= V; dest++) {
 					if(matrix[source][dest] != Integer.MAX_VALUE) {
@@ -32,22 +33,22 @@ public class Bellman_Ford {
 		}
 		return d;
 	}
-	public int[] BellmanFord_List(int start, int list) {
+	public int[] BellmanFord_List(int start, ArrayList<Edge>[] list) {
 		Arrays.fill(d, Integer.MAX_VALUE);
 		d[start] = 0;
 		for(int i = 0; i < V - 1; i++) {
-			for(list<pair of integers> edges : list>) {
-				for(<pair of integers> edge : edges) {
-					if(d[i] + edge.weight < d[edge.end]) {
-						d[edge.end] = d[i] + edge.weight;
+			for(ArrayList<Edge> edges : list) {
+				for(Edge edge : edges) {
+					if(d[i] + edge.weight < d[edge.vertex]) {
+						d[edge.vertex] = d[i] + edge.weight;
 					}
 				}
 			}
 		}
 		for(int i = 0; i < V - 1; i++) {
-			for(list<pair of integers> edges : list>) {
-				for(<pair of integers> edge : edges) {
-					if(d[i] + edge.weight < d[edge.end]) {
+			for(ArrayList<Edge> edges : list) {
+				for(Edge edge : edges) {
+					if(d[i] + edge.weight < d[edge.vertex]) {
 						System.out.println("Negative cycle");
 					}
 				}
